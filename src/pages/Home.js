2a932b190d37ff60b5d84b510b5b7b1addfde2af
Yelsultan Niyaztay <niyaztaye@gmail.com>
2023-03-12 21:19:02 +0600

    Finished landing page design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,11 @@ import {React} from 'react';
 import Main from './components/Main';
 import Page2 from './components/Page2';
 import Page3 from './components/Page3';
+import Page4 from './components/Page4';
+import Page5 from './components/Page5';
+import Page6 from './components/Page6';
+import Page7 from './components/Page7';
+import Footer from './components/Footer';
 
 
 const Home = () => {
@@ -11,6 +16,11 @@ const Home = () => {
             <Main />
             <Page2 />
             <Page3 />
+            <Page4 />
+            <Page5 />
+            <Page6 />
+            <Page7 />
+            <Footer />
         </div>
     )
 }
--- src/pages/components/Page2.js
+++ src/pages/components/Page2.js
@@ -1,40 +1,56 @@
 const Page2 = () => (
-    <div className="flex flex-col items-center bg-[#EBF0FEBF] h-screen w-full">
-        <div className="py-24 flex flex-col items-center justify-center w-full">
-            <div className="font-inter text-[50px] w-[650px] items-center justify-center text-center font-semibold mb-40">
+    <div className="flex flex-col items-center bg-[#EBF0FEBF] w-full">
+        <div className="py-[100px] flex flex-col items-center justify-center w-full">
+            <div className="font-inter text-[75px] w-[960px] items-center justify-center text-center font-semibold mb-40 relative">
                 Spend less time worrying and more time living life.
+                <div className="absolute bottom-[10px] right-[40px]">
+                    <svg width="355" height="16" viewBox="0 0 355 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M16.5288 4.33121C8.48961 4.77339 3.70038 5.17131 2.93067 5.61349C1.04916 6.58628 4.81206 7.07285 16.6998 7.11707C16.7853 7.11707 325.439 3.49111 325.525 3.49111C339.465 3.18159 340.577 2.78371 340.577 2.03201C340.577 1.05921 336.814 0.395772 328.775 0.0862465C318.17 -0.400151 75.9676 1.19174 16.5288 4.33121ZM330.742 6.54211C326.979 6.63054 82.0399 9.54888 75.8822 9.81419C0.450546 12.6884 -5.36505 13.1307 2.75967 15.2531C6.69374 16.2701 14.1343 16.27 46.3766 15.1203C124.117 12.423 345.452 12.1137 353.234 10.6545C356.826 9.94702 357.34 6.05571 330.742 6.54211Z" fill="#E868C4"/>
+                    </svg>  
+                </div>
             </div>
             <div className="flex flex-row justify-around w-full">
-                <div className="flex flex-col justify-center items-center text-center w-[400px]">
-                    <div className="w-[64px] h-[64px] mb-8">
-                        <img src="https://picsum.photos/64/64" alt=""/>
+                <div className="flex flex-col justify-center items-center text-center w-[525px]">
+                    <div className="w-[64px] h-[64px] mb-[45px]">
+                        <svg width="71" height="72" viewBox="0 0 71 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="64" height="64" rx="20" fill="#381C46"/>
+                            <path d="M52.5253 55.0495C56.7124 54.2437 59.2125 49.9134 57.8168 45.8843L56.3847 41.7502C54.4216 36.0833 46.7067 35.3477 43.7081 40.5415L40.8439 45.5024C37.8452 50.6962 42.3397 57.0097 48.2289 55.8763L52.5253 55.0495Z" fill="#E868C4"/>
+                        </svg>
                     </div>
-                    <div className="font-amiri text-[32px] font-bold mb-8">
+                    <div className="font-amiri text-[40px] font-bold mb-[45px]">
                         Calculate your budget
                     </div>
-                    <div className="font-amiri text-[24px] font-normal">
-                        We give you the opportunity to set goals and calculate your budget. With this you can regularly share behind the process.
-                    </div>
-                </div>
-                <div className="flex flex-col justify-center items-center text-center w-[400px]">
-                    <div className="w-[64px] h-[64px] mb-8">
-                        <img src="https://picsum.photos/64/64" alt=" "/>
-                    </div>
-                    <div className="font-amiri text-[32px] font-bold mb-8">
-                        Generate monthly report 
-                    </div>
-                    <div className="font-amiri text-[24px] font-normal">
+                    <div className="font-amiri text-[32px] font-normal leading-normal">
                         You can create your own weekly/monthly reports. This will help you see the full status of your finance.
                     </div>
                 </div>
-                <div className="flex flex-col justify-center items-center text-center w-[400px]">
-                    <div className="w-[64px] h-[64px] mb-8">
-                        <img src="https://picsum.photos/64/64" alt=""/>
+                <div className="flex flex-col justify-center items-center text-center w-[525px]">
+                    <div className="w-[64px] h-[64px] mb-[45px]">
+                        <svg width="71" height="72" viewBox="0 0 71 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="64" height="64" rx="20" fill="#381C46"/>
+                            <path d="M52.5253 55.0495C56.7124 54.2437 59.2125 49.9134 57.8168 45.8843L56.3847 41.7502C54.4216 36.0833 46.7067 35.3477 43.7081 40.5415L40.8439 45.5024C37.8452 50.6962 42.3397 57.0097 48.2289 55.8763L52.5253 55.0495Z" fill="#E868C4"/>
+                        </svg>
+
                     </div>
-                    <div className="font-amiri text-[32px] font-bold mb-8">
-                        Real-time tracker
+                    <div className="font-amiri text-[40px] font-bold mb-[45px]">
+                        Generate monthly report 
                     </div>
-                    <div className="font-amiri text-[24px] font-normal">
+                    <div className="font-amiri text-[32px] font-normal leading-normal">
+                        You can create your own weekly/monthly reports. This will help you see the full status of your finance. 
+                    </div>
+                </div>
+                <div className="flex flex-col justify-center items-center text-center w-[525px]">
+                    <div className="w-[64px] h-[64px] mb-[45px]">
+                        <svg width="71" height="72" viewBox="0 0 71 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="64" height="64" rx="20" fill="#381C46"/>
+                            <path d="M52.5253 55.0495C56.7124 54.2437 59.2125 49.9134 57.8168 45.8843L56.3847 41.7502C54.4216 36.0833 46.7067 35.3477 43.7081 40.5415L40.8439 45.5024C37.8452 50.6962 42.3397 57.0097 48.2289 55.8763L52.5253 55.0495Z" fill="#E868C4"/>
+                        </svg>
+
+                    </div>
+                    <div className="font-amiri text-[40px] font-bold mb-[45px]">
+                        Real-time tracker 
+                    </div>
+                    <div className="font-amiri text-[32px] font-normal leading-normal">
                         Real-time expense tracking allows you to properly plan your expenses and adapt to changing needs.
                     </div>
                 </div>
