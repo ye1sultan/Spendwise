be03406701015875707b582e0cd71723b8570030
Yelsultan Niyaztay <niyaztaye@gmail.com>
2023-04-08 00:23:31 +0600

    Landing responsive v1

--- src/pages/landing/Header.js
+++ src/pages/landing/Header.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 import Button from "../components/Button";
-import { AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 const Header = () => {
+
+    const [menu, setMenu] = useState(false);
+
     const handleClickScroll = (e) => {
         const id = e.currentTarget.getAttribute('data-id');
         const element = document.getElementById(id);
@@ -11,12 +16,12 @@ const Header = () => {
     }
 
     const handleClick = () => {
-
+        menu === true ? setMenu(false) : setMenu(true);
     }
 
     return (
-        <div id='header' className='flex justify-center 2xl:justify-between items-center w-full 2xl:max-w-[1690px] 2xl:mx-auto h-16 2xl:h-40 px-4 bg-white 2xl:bg-transparent relative'>
-            <div className='text-4xl w-full font-semibold'>LOGO</div>
+        <div id='header' className='flex justify-center 2xl:justify-between items-center w-full 2xl:max-w-[1690px] 2xl:mx-auto h-16 2xl:h-40 px-4 bg-white 2xl:bg-transparent relative z-50'>
+            <div className='text-3xl w-full font-semibold z-50'>LOGO</div>
             <div className='hidden 2xl:flex flex-row justify-between items-center w-full'>
                 <div className="flex flex-row justify-between ml-[-100px]">
                     <button data-id='about' className="text-[24px] font-light mr-[50px]" onClick={handleClickScroll}>About Us</button>
@@ -30,24 +35,24 @@ const Header = () => {
                 </div>
             </div>
 
-            <div className="flex 2xl:hidden flex-col justify-center items-center absolute left-0 top-0 pt-3 pl-4 bg-white h-screen w-[100%] z-50">
-                <div className='text-4xl w-full font-semibold'>LOGO</div>
-                <div className='flex flex-col justify-between items-center'>
-                    <div className="flex flex-col justify-between h-[200px]">
-                        <button data-id='about' className="text-[24px] font-light mr-[50px]" onClick={handleClickScroll}>About Us</button>
-                        <button data-id='tracking' className="text-[24px] font-light mr-[50px]" onClick={handleClickScroll}>Tracking</button>
-                        <button data-id='report' className="text-[24px] font-light mr-[50px]" onClick={handleClickScroll}>Report</button>
-                        <button data-id='support' className="text-[24px] font-light mr-[50px]" onClick={handleClickScroll}>Support</button>
+            <div className={`flex 2xl:hidden flex-col justify-center items-center absolute left-0 top-0 pt-3 bg-white h-screen w-[100%] z-40 ${menu === true ? 'translate-x-0' : 'translate-x-[-100%]'} transition-transform ease-in-out duration-300`}>
+                <div className='flex flex-col justify-center items-center w-full'>
+                    <div className="flex flex-col justify-between items-center h-[200px]">
+                        <button data-id='about' className="text-[24px] font-light" onClick={handleClickScroll}>About Us</button>
+                        <button data-id='tracking' className="text-[24px] font-light" onClick={handleClickScroll}>Tracking</button>
+                        <button data-id='report' className="text-[24px] font-light" onClick={handleClickScroll}>Report</button>
+                        <button data-id='support' className="text-[24px] font-light" onClick={handleClickScroll}>Support</button>
                     </div>
-                    <div className="flex flex-col pt-5">
+                    <div className="flex flex-col pt-6 justify-center items-center w-full gap-2">
                         <Button route='/signin' text='Sign In' width='w-[110px]' height='h-[48px]' size='text-[24px]' color='text-black' bold='font-light' font='font-inter' display='block' />
                         <Button route='/signup' text='Sign Up' width='w-[110px]' height='h-[48px]' bg='bg-[#EFCA47CC]' rounded='rounded-[8px]' size='text-[24px]' color='text-black' bold='font-light' font='font-inter' display='block' />
                     </div>
                 </div>
             </div>
 
-            <button onClick={handleClick}>
-                <AiOutlineMenu size={40} className="flex 2xl:hidden" />
+            <button className="flex 2xl:hidden z-50" onClick={handleClick}>
+                <AiOutlineMenu size={30} className={`${menu === true ? 'hidden' : 'flex'}`} />
+                <AiOutlineClose size={30} className={`${menu === true ? 'flex' : 'hidden'}`} />
             </button>
         </div>
     );
