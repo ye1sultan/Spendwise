7a3c14b9c545f730124526b765fe86eb48fadeaf
Yelsultan Niyaztay <niyaztaye@gmail.com>
2023-04-28 20:58:52 +0600

    Made my profile logic work

--- src/pages/application/Folder/settings/MyProfile.js
+++ src/pages/application/Folder/settings/MyProfile.js
@@ -2,18 +2,30 @@ import { useState } from "react";
 import { BiPencil } from "react-icons/bi";
 
 const MyProfile = () => {
-    const User = {
-        name: 'Zholshieva Arailym',
-        email: 'azholshiyeva@gmail.com',
+    const [user, setUser] = useState({
+        name: 'Niyaztay Yelsultan',
+        email: 'niyaztaye@gmail.com',
         avatar: "https://picsum.photos/200/200",
-    };
+    });
 
-    const [name, setName] = useState(User.name);
-    const [email, setEmail] = useState(User.email);
-    const [avatar, setAvatar] = useState(User.avatar);
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const [avatar, setAvatar] = useState(user.avatar);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const [nameChanged, setNameChanged] = useState(false);
+    const [avatarChanged, setAvatarChanged] = useState(false);
+
+    const handleSubmit = () => {
+        if (nameChanged || avatarChanged) {
+            setUser({ ...user, name, avatar });
+            setNameChanged(false);
+            setAvatarChanged(false);
+        }
+    }
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+        setNameChanged(e.target.value !== user.name);
     }
 
     const handleAvatarChange = (e) => {
@@ -21,10 +33,11 @@ const MyProfile = () => {
             const reader = new FileReader();
             reader.onload = (event) => {
                 setAvatar(event.target.result);
+                setAvatarChanged(true);
             };
             reader.readAsDataURL(e.target.files[0]);
         }
-    };
+    }
 
     return (
         <div className="self-start w-[970px] bg-white rounded-[30px] border-[1px] border-[#AEAEAE] px-[45px] py-[25px] flex justify-center items-center">
@@ -32,13 +45,29 @@ const MyProfile = () => {
                 <div className="text-[40px] font-medium mb-8">
                     Account Details
                 </div>
-                <form onSubmit={handleSubmit} className="flex flex-row justify-center items-start">
+                <div className="flex flex-row justify-center items-start">
                     <div className="flex flex-col justify-center items-start">
                         <label className="text-[24px] font-medium mb-[10px]">Name</label>
-                        <input className="w-[490px] h-[50px] border-b-[1px] border-[#000] mb-[35px] text-[24px] font-medium" type="text" placeholder="Your name" value={name} onChange={e => setName(e.target.value)} required />
+                        <input
+                            className="w-[490px] h-[50px] border-b-[1px] border-[#000] mb-[35px] text-[24px] font-medium"
+                            type="text"
+                            placeholder="Your name"
+                            value={name}
+                            onChange={handleNameChange}
+                            required />
                         <label className="text-[24px] font-medium mb-[10px]">Email</label>
-                        <input className="w-[490px] h-[50px] border-b-[1px] border-[#000] mb-[35px] text-[24px] text-[#a0a0a0] font-medium" type="email" placeholder="Your email" value={email} onChange={e => setEmail(e.target.value)} readOnly />
-                        <input className="w-[340px] h-[50px] rounded-[30px] bg-[#D9D9D9] bg-opacity-30 mt-[70px] cursor-pointer text-[16px] font-medium uppercase text-[#000] text-opacity-50" type="submit" value="Update your information" />
+                        <input
+                            className="w-[490px] h-[50px] border-b-[1px] border-[#000] mb-[35px] text-[24px] text-[#a0a0a0] font-medium cursor-not-allowed"
+                            type="email"
+                            placeholder="Your email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            readOnly />
+                        <input
+                            onClick={() => handleSubmit()}
+                            className={`w-[340px] h-[50px] rounded-[30px] ${(nameChanged || avatarChanged) ? "bg-[#9F75D6] text-[#fff] cursor-pointer" : "bg-[#D9D9D9] bg-opacity-30 cursor-not-allowed"} mt-[70px] cursor-pointer text-[16px] font-medium uppercase`}
+                            type="submit"
+                            value="Update your information" />
                     </div>
                     <div className="w-full h-full flex justify-center items-center">
                         <div className="w-[200px] h-[200px] relative">
@@ -57,7 +86,7 @@ const MyProfile = () => {
                             />
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
         </div >
     );
