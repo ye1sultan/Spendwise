3384653e54bde834a3805ad30842702c375c4f0a
Yelsultan Niyaztay <niyaztaye@gmail.com>
2023-05-11 01:15:15 +0600

    Responsive Header v1

--- src/pages/application/components/Header.js
+++ src/pages/application/components/Header.js
@@ -5,19 +5,19 @@ const Header = () => {
     return (
         <div className="w-full flex flex-row justify-between items-start">
             <div className="flex flex-col">
-                <div className="text-[14px] 2xl:text-[24px]">
+                <div className="text-sm md:text-base lg:text-lg xl:text-[20px] 2xl:text-[24px] mb-[5px]">
                     The Complete Wallet
                 </div>
-                <div className="text-[18px] 2xl:text-[36px] font-medium">
+                <div className="text-base md:text-lg lg:text-xl xl:text-[28px] 2xl:text-[36px] font-medium">
                     Manage Your <span className="font-bold">Finance</span>
                 </div>
             </div>
             <div className="flex flex-row justify-center items-center">
                 <div className="flex flex-row ml-[20px]">
-                    {avt ? <img className='rounded-full w-[30px] h-[30px] 2xl:w-[45px] 2xl:h-[45px]' src={''} alt='avatar' /> : <div className='rounded-full bg-blue-200 flex justify-center items-center w-[30px] h-[30px] 2xl:w-[45px] 2xl:h-[45px] text-[25px] font-medium'>
+                    {avt ? <img className='rounded-full w-[30px] h-[30px] lg:w-[45px] lg:h-[45px]' src={''} alt='avatar' /> : <div className='rounded-full bg-blue-200 flex justify-center items-center w-[30px] h-[30px] lg:w-[45px] lg:h-[45px] lg:text-[25px] font-medium'>
                         {name.charAt(0) || ''}
                     </div>}
-                    <div className="hidden 2xl:flex justify-center items-center text-[20px] font-semibold ml-[20px]">
+                    <div className="flex justify-center items-center text-base md:text-lg lg:text-xl font-semibold ml-[10px] lg:ml-[20px]">
                         {name || 'Not registered'}
                     </div>
                 </div>
